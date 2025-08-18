8b5b5fc40bdfe2945facfad775d100e00bce3c90
Gian Machella <gianmachella@Gians-MacBook-Pro.local>
2025-08-18 11:32:43 -0500

    BE: CORS con preflight y www

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const loginUser = async (req, res) => {
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
-    console.log("Resultado de la comparación:", isMatch);
+    console.log("Resultado de la comparación de contraseñas:", isMatch);
     if (!isMatch) {
       return res.status(401).json({ message: "Invalid password" });
     }
