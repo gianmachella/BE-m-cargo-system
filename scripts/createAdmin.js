ccd4f67588c0b4743e42b50f6010e4fb521b8799
Gian Machella <gianmachella@Gians-MacBook-Pro.local>
2025-08-21 09:58:53 -0500

    script users create

--- scripts/createAdmin.js
+++ scripts/createAdmin.js
@@ -1,26 +1,56 @@
+// scripts/createUsers.js
 const bcrypt = require("bcrypt");
 const { sequelize } = require("../models");
 const User = require("../models/User");
 
 (async () => {
   try {
-    const hashedPassword = await bcrypt.hash("Dev2020!!", 10);
+    const users = [
+      {
+        firstName: "Ruben",
+        lastName: "Soto",
+        userName: "rsoto",
+        email: "rsoto@globalcargous.com",
+        password: "GC2020utah..",
+        company: "global-cargo",
+      },
+      {
+        firstName: "Rossana",
+        lastName: "Hernandez",
+        userName: "rhernandez",
+        email: "rhernandez@globalcargous.com",
+        password: "Hernandez2020!!",
+        company: "global-cargo",
+      },
+      {
+        firstName: "Anny",
+        lastName: "Castilla",
+        userName: "acastilla",
+        email: "acastilla@globalcargous.com",
+        password: "Castilla2025..",
+        company: "global-cargo",
+      },
+    ];
 
-    const admin = await User.create({
-      firstName: "Admin",
-      lastName: "System",
-      userName: "admin",
-      email: "admin@globalcontrol-system.com",
-      password: hashedPassword,
-      company: "global-cargo",
-      createdBy: 0,
-      updatedBy: 0,
-    });
+    for (const u of users) {
+      const hashedPassword = await bcrypt.hash(u.password, 10);
 
-    console.log("✅ Admin creado:", admin.toJSON());
+      const newUser = await User.create({
+        firstName: u.firstName,
+        lastName: u.lastName,
+        userName: u.userName,
+        email: u.email,
+        password: hashedPassword,
+        company: u.company,
+        createdBy: 0,
+        updatedBy: 0,
+      });
+
+      console.log(`✅ Usuario creado: ${u.email}`);
+    }
   } catch (error) {
-    console.error("❌ Error creando admin:", error);
+    console.error("❌ Error creando usuarios:", error);
   } finally {
-    await sequelize.close(); // 👈 ahora sí definido
+    await sequelize.close();
   }
 })();
